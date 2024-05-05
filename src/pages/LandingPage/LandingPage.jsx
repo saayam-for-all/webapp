5b4d8d078cc0d2504da6a1870a43c6e8ff733278
savan15k <savan.kared23@gmail.com>
2024-05-05 16:03:09 -0700

    added code for image rotation on Landingpage

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,14 +1,16 @@
 import "./LandingPage.css";
 import { useTranslation } from "react-i18next";
-import Carousel from "./components/Carousel";
+//import Carousel from "./components/Carousel";
 import HeroSection from "./components/HeroSection";
 import Info from "./components/Info";
+import { EmblaCarousel } from "./components/emblacarousel";
 
 export default function Home() {
 	const { t } = useTranslation();
 
 	return (
 		<div>
+			<EmblaCarousel/>
 			{/* <div className='w-[85%] mx-auto'>
 				<Carousel />
 			</div> */}
