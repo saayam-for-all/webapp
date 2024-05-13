efc6bf3e521e87735ecd800584ab78e1d4d2365d
savan15k <125933088+savan15k@users.noreply.github.com>
2024-05-13 08:01:44 -0700

    image_rotation_second_trial

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,19 +1,15 @@
 import "./LandingPage.css";
-import { useTranslation } from "react-i18next";
-//import Carousel from "./components/Carousel";
 import HeroSection from "./components/HeroSection";
 import Info from "./components/Info";
-import { EmblaCarousel } from "./components/emblacarousel";
+import Images from "./components/Dynamic_img.jsx";
 
 export default function Home() {
-	const { t } = useTranslation();
+	// const { t } = useTranslation();
 
 	return (
 		<div>
-			<EmblaCarousel/>
-			{/* <div className='w-[85%] mx-auto'>
-				<Carousel />
-			</div> */}
+			<myElement/>
+			<Images/>
 			<HeroSection />
 			<Info />
 		</div>
