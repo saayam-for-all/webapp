c02e88519c5c91e72f6ab337adeef02f90ef664f
RajeshMahendran <rajeshmahendran1999@gmail.com>
2024-06-23 00:39:04 -0500

    Navbar after user signs in

--- src/common/components/Navbar/Navbar.jsx
+++ src/common/components/Navbar/Navbar.jsx
@@ -1,15 +1,43 @@
-import "./NavBar.css";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import "./NavBar.css";
-import { Link, NavLink } from "react-router-dom";
-import languages from "../../i18n/languagesData";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import LOGO from "../../../assets/logo.svg";
+import DEFAULT_PROFILE_ICON from "../../../assets/Landingpage_images/ProfileImage.jpg";
+import "./NavBar.css";
 
 const Navbar = () => {
   const { i18n, t } = useTranslation();
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [profileIcon, setProfileIcon] = useState(DEFAULT_PROFILE_ICON);
+  const fileInputRef = useRef(null);
+  const profileDropdownRef = useRef(null);
+  const location = useLocation();
 
+  useEffect(() => {
+    if (location.pathname === "/dashboard") {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, [location.pathname]);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        profileDropdownRef.current &&
+        !profileDropdownRef.current.contains(event.target)
+      ) {
+        setIsProfileDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   const handleDropdownClick = () => {
     setIsDropdownOpen(!isDropdownOpen);
@@ -33,6 +61,31 @@ const Navbar = () => {
 
     window.location.href = cognitoUrl;
   };
+
+  const handleProfileClick = () => {
+    setIsProfileDropdownOpen(!isProfileDropdownOpen);
+  };
+
+  const handleLogout = () => {
+    window.location.href = window.location.origin;
+  };
+
+  const handleEditProfilePicture = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleProfilePictureChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setProfileIcon(e.target.result);
+      };
+      reader.readAsDataURL(file);
+      setIsProfileDropdownOpen(false);
+    }
+  };
+
   return (
     <header>
       <div className="navbar navbar-sm navbar-gradient-bg s">
@@ -43,63 +96,96 @@ const Navbar = () => {
         </div>
 
         <div className="navbar-end gap-10">
-          <div className="dropdown">
-            <div
-              tabIndex={0}
-              className={`cursor-pointer font-semibold ${
-                isDropdownOpen ? "active" : ""
-              }`}
-              onClick={handleDropdownClick}
-            >
-              {t("about")}
-            </div>
-            <ul
-              tabIndex={0}
-              className={`menu menu-md dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 ${
-                isDropdownOpen ? "block" : "hidden"
-              }`}
-            >
-              <li>
-                <a onClick={handleLinkClick}>
-                  <NavLink to="/directors">{t("directors")}</NavLink>
-                </a>
-              </li>
-              <li>
-                <NavLink to="/how-we-operate" onClick={handleLinkClick}>
-                  {t("how_we_operate")}
-                </NavLink>
-              </li>
-              <li>
-                <a onClick={handleLinkClick}>
-                  <NavLink to="/contact">{t("contact")}</NavLink>
-                </a>
-              </li>
-              <li>
-                <a onClick={handleLinkClick}>
-                  <NavLink to="/mission">{t("mission")}</NavLink>
-                </a>
-              </li>
-              <li>
-                <a onClick={handleLinkClick}>
-                  <NavLink to="/vision">{t("vision")}</NavLink>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <NavLink to="/donate" className="font-semibold" onClick={handleLinkClick}>
-            {t("donate")}
-          </NavLink>
-          <button className="font-semibold" onClick={handleLogin}>{t("login")}</button>
-          {/* <select
-            className='p-1 outline-none rounded-lg'
-            onChange={(e) => i18n.changeLanguage(e.target.value)}
-          >
-            {languages?.map((language) => (
-              <option key={language.code} value={language.code}>
-                {language.name}
-              </option>
-            ))}
-          </select> */}
+          {isLoggedIn ? (
+            <>
+              <div className="relative" ref={profileDropdownRef}>
+                <img
+                  src={profileIcon}
+                  alt="Profile Icon"
+                  className="w-8 h-8 rounded-full cursor-pointer"
+                  onClick={handleProfileClick}
+                />
+                {isProfileDropdownOpen && (
+                  <ul className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-lg z-10">
+                    <li className="p-2 hover:bg-gray-100 cursor-pointer" onClick={handleEditProfilePicture}>
+                      {t("Profile")}
+                    </li>
+                    <li className="p-2 hover:bg-gray-100 cursor-pointer" onClick={handleLogout}>
+                      {t("Logout")}
+                    </li>
+                  </ul>
+                )}
+                <input
+                  type="file"
+                  ref={fileInputRef}
+                  style={{ display: "none" }}
+                  onChange={handleProfilePictureChange}
+                />
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="dropdown">
+                <div
+                  tabIndex={0}
+                  className={`cursor-pointer font-semibold ${
+                    isDropdownOpen ? "active" : ""
+                  }`}
+                  onClick={handleDropdownClick}
+                >
+                  {t("about")}
+                </div>
+                <ul
+                  tabIndex={0}
+                  className={`menu menu-md dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 ${
+                    isDropdownOpen ? "block" : "hidden"
+                  }`}
+                >
+                  <li>
+                    <a onClick={handleLinkClick}>
+                      <NavLink to="/directors">{t("directors")}</NavLink>
+                    </a>
+                  </li>
+                  <li>
+                    <NavLink to="/how-we-operate" onClick={handleLinkClick}>
+                      {t("how_we_operate")}
+                    </NavLink>
+                  </li>
+                  <li>
+                    <a onClick={handleLinkClick}>
+                      <NavLink to="/contact">{t("contact")}</NavLink>
+                    </a>
+                  </li>
+                  <li>
+                    <a onClick={handleLinkClick}>
+                      <NavLink to="/mission">{t("mission")}</NavLink>
+                    </a>
+                  </li>
+                  <li>
+                    <a onClick={handleLinkClick}>
+                      <NavLink to="/vision">{t("vision")}</NavLink>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <NavLink to="/donate" className="font-semibold" onClick={handleLinkClick}>
+                {t("donate")}
+              </NavLink>
+              <button className="font-semibold" onClick={handleLogin}>
+                {t("login")}
+              </button>
+              {/* <select
+                className='p-1 outline-none rounded-lg'
+                onChange={(e) => i18n.changeLanguage(e.target.value)}
+              >
+                {languages?.map((language) => (
+                  <option key={language.code} value={language.code}>
+                    {language.name}
+                  </option>
+                ))}
+              </select> */}
+            </>
+          )}
         </div>
       </div>
     </header>
