18daec513367febe362d4c1441edcd92658c315e
Gourav Manna <gouravmanna3@gmail.com>
2024-07-23 11:52:13 -0600

    fix: make table rows content dynamic

--- src/common/components/DataTable/Table.jsx
+++ src/common/components/DataTable/Table.jsx
@@ -5,34 +5,26 @@ const Table = ({ headers, rows }) => {
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
             {headers.map((header) => (
-              <th key={header.id} scope="col" class="px-6 py-3">
-                {header.name}
+              <th key={header} scope="col" class="px-6 py-3">
+                {header}
               </th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {rows.map((row) => {
-            const { id, requestType, creationDate, closedDate, subject } = row;
-
-            return (
-              <tr
-                key={id}
-                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
-              >
-                <th
-                  scope="row"
-                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  {id}
-                </th>
-                <td class="px-6 py-4">{requestType}</td>
-                <td class="px-6 py-4">{subject}</td>
-                <td class="px-6 py-4">{creationDate}</td>
-                <td class="px-6 py-4 text-right">{closedDate}</td>
-              </tr>
-            );
-          })}
+          {rows.map((row, rowIndex) => (
+            /* key must be changed if delete or update row is implemented */
+            <tr
+              key={rowIndex}
+              class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+            >
+              {headers.map((header, colIndex) => (
+                <td key={colIndex} class="px-6 py-4">
+                  {row[header]}
+                </td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
