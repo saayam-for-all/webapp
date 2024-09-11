d387b686889a6318ec0764f241bbd02f3e340935
Dikshita Kejriwal <djk5958@psu.edu>
2024-09-11 12:16:42 -0500

    Implemented Request Voluntary Organizations Button

--- src/pages/RequestDetails/RequestDetails.jsx
+++ src/pages/RequestDetails/RequestDetails.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import CommentsSection from "./CommentsSection";
 import RequestDescription from "./RequestDescription";
 import RequestDetailsSidebar from "./RequestDetailsSidebar";
@@ -7,6 +8,15 @@ const RequestDetails = () => {
     <div className="m-8 grid grid-cols-10 gap-4">
       <div className="col-span-7">
         <RequestDescription />
+
+        <div className="mt-4">
+          <Link to="/voluntary-organizations">
+            <button className="bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700">
+              Request Voluntary Organizations
+            </button>
+          </Link>
+        </div>
+
         <CommentsSection />
       </div>
       <div className="col-span-3">
