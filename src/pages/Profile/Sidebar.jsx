1355f6e9e7df8736dafbe1a5c7123ed200540637
deepika-venkatesan <deepikajvenkatesan@gmail.com>
2024-09-19 10:10:53 -0500

    Add logout functionality in profile page

--- src/pages/Profile/Sidebar.jsx
+++ src/pages/Profile/Sidebar.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { FaUserCircle, FaLock, FaPencilAlt } from 'react-icons/fa';
+import { FaUserCircle, FaLock, FaPencilAlt, FaSignOutAlt } from 'react-icons/fa';
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logout } from "../../redux/features/authentication/authActions";
 
 function Sidebar({ profilePhoto, userName, userEmail, handleTabChange, activeTab, openModal }) {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const handleSignOut = () => {
+        dispatch(logout());
+    };
+
     return (
-        <div className="w-1/4 flex flex-col items-center p-4 border-r">
-            <div className="mt-6 w-full text-center">
+        <div className="flex flex-col justify-between h-full p-4 border-r"> {/* Full-height layout */}
+            <div className="text-center">
                 <div className="relative mb-4">
                     <img
                         src={profilePhoto}
@@ -23,8 +33,8 @@ function Sidebar({ profilePhoto, userName, userEmail, handleTabChange, activeTab
                 <p className="text-gray-500">{userEmail}</p>
             </div>
 
-            
-            <div className="mt-6 w-full">
+            {/* Profile and other tabs */}
+            <div className="mt-6">
                 <button
                     className={`flex items-center py-2 px-4 text-left w-full mb-2 ${
                         activeTab === 'profile'
@@ -64,6 +74,15 @@ function Sidebar({ profilePhoto, userName, userEmail, handleTabChange, activeTab
                     onClick={() => handleTabChange('organization')}
                 >
                     <FaUserCircle className="mr-2" /> Organization Details
+                </button>
+            </div>
+
+            <div className="mt-8">
+                <button
+                    className="flex items-center justify-center w-full py-3 px-4 text-white bg-blue-500 hover:bg-blue-600 font-semibold rounded-md"
+                    onClick={handleSignOut}
+                >
+                    <FaSignOutAlt className="mr-2 text-lg" /> Log out
                 </button>
             </div>
         </div>
