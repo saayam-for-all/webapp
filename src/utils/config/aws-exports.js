df4caa16860df2f8a55fcf12efaa837133cc7536
Gourav Manna <gouravmanna3@gmail.com>
2024-11-24 23:18:49 -0500

    feat: add firstname and lastname field in signup page

--- src/utils/config/aws-exports.js
+++ src/utils/config/aws-exports.js
@@ -5,8 +5,8 @@ const awsConfig = {
     Cognito: {
       identityPoolId: "us-east-1:d43f18d8-08e1-4444-8ad3-43eb18540540",
       region: "auth.us-east-1",
-      userPoolId: "us-east-1_wreskjlwM",
-      userPoolClientId: "4s7eu3ctd5lh0pc25odl3spuc7",
+      userPoolId: "us-east-1_kbJhasUll",
+      userPoolClientId: "58tqba3bjsk2v90kavksbqptrm",
       loginWith: {
         email: true,
       },
