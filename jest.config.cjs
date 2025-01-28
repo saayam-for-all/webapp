c30c36f307c6782cfb216da1c1bc856558762788
Leif Gaebler <Lorkenpeist@gmail.com>
2025-01-27 19:59:33 -0800

    Remove axios from moduleNameMapper

--- jest.config.cjs
+++ jest.config.cjs
@@ -8,7 +8,6 @@ module.exports = {
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/__mocks__/fileMock.js",
-    "^axios$": "axios/dist/node/axios.cjs",
   },
   testEnvironment: "jsdom",
 };
