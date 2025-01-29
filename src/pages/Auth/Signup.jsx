7f14e10ff169c046587568360b2447457ce45d59
Likhith H G <likhith2098@gmail.com>
2025-01-29 14:59:34 -0700

    Removed unnecessary lines that I used for testing

--- src/pages/Auth/Signup.jsx
+++ src/pages/Auth/Signup.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
 import { FaFacebookF } from "react-icons/fa";
