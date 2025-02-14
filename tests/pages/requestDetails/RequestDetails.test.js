d543a6a3ea4f803f8dad4533604271f810ef4849
manchikantisowmya <sowmya.manchikanti@sjsu.edu>
2025-02-13 19:43:58 -0800

    correct filename

--- tests/pages/requestDetails/RequestDetails.test.js
+++ tests/pages/requestDetails/RequestDetails.test.js
@@ -5,9 +5,8 @@ import { createStore } from "redux";
 import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
 import RequestDetails from "../../../src/pages/RequestDetails/RequestDetails";
-import { getMyRequests } from "../../../src/services/requestServices";
 
-jest.mock("../../../src/services/requestservices", () => ({
+jest.mock("../../../src/services/requestServices", () => ({
   getMyRequests: jest.fn(() =>
     Promise.resolve({ message: "Mocked API Response" }),
   ),
