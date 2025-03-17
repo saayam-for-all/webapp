c0e261a028581cf0771180d55d6b9e234e32b9c1
Bhanu <bhanutejat2210032gmail.com>
2025-03-17 15:17:40 -0500

    Change the colour of editable fields in profile tabs

--- src/pages/Profile/ChangePassword.jsx
+++ src/pages/Profile/ChangePassword.jsx
@@ -78,7 +78,7 @@ function ChangePassword({ setHasUnsavedChanges }) {
                 type={showPassword ? "text" : "password"}
                 value={currentPassword}
                 onChange={(e) => setCurrentPassword(e.target.value)}
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                className="appearance-none block w-full bg-white-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               />
               <div
                 onClick={() => setShowPassword(!showPassword)}
@@ -97,7 +97,7 @@ function ChangePassword({ setHasUnsavedChanges }) {
                 type={showPassword ? "text" : "password"}
                 value={newPassword}
                 onChange={(e) => setNewPassword(e.target.value)}
-                className={`appearance-none block w-full bg-gray-200 text-gray-700 border ${
+                className={`appearance-none block w-full bg-white-200 text-gray-700 border ${
                   errorMessage ? "border-red-500" : "border-gray-200"
                 } rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`}
               />
@@ -124,7 +124,7 @@ function ChangePassword({ setHasUnsavedChanges }) {
                 type={showPassword ? "text" : "password"}
                 value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
-                className={`appearance-none block w-full bg-gray-200 text-gray-700 border ${
+                className={`appearance-none block w-full bg-white-200 text-gray-700 border ${
                   passwordMatchError ? "border-red-500" : "border-gray-200"
                 } rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`}
               />
