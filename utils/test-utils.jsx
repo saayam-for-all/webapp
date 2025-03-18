44ff5eecde0132b26c178843e72cb469bdf83a57
Leif Gaebler <Lorkenpeist@gmail.com>
2025-03-17 19:56:55 -0700

    Mock logged in/out user for Navbar

--- utils/test-utils.jsx
+++ utils/test-utils.jsx
@@ -1,8 +1,26 @@
 import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
-import { store } from "#redux/store";
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "#redux/features/authentication/authSlice";
 
-export function renderWithProviders(ui, renderOptions = {}) {
+// redux state for logged in user
+export const MOCK_STATE_LOGGED_IN = { auth: { user: { userId: "mockUser" } } };
+// redux state for logged out user
+export const MOCK_STATE_LOGGED_OUT = { auth: { user: null } };
+
+export function renderWithProviders(ui, extendedRenderOptions = {}) {
+  const {
+    preloadedState = {},
+    // Automatically create a store instance if no store was passed in
+    store = configureStore({
+      reducer: {
+        auth: authReducer,
+      },
+      preloadedState,
+    }),
+    ...renderOptions
+  } = extendedRenderOptions;
+
   const Wrapper = ({ children }) => (
     <Provider store={store}>{children}</Provider>
   );
