2a65126235e54be30752f92b968de69661279e43
Chinmai Sai Kolla <chinmaisaikolla@macbookair.lan>
2025-04-07 14:56:16 -0500

    Updated Test case

--- src/pages/Profile/your-profile.test.js
+++ src/pages/Profile/your-profile.test.js
@@ -6,7 +6,11 @@ import { MOCK_STATE_LOGGED_IN, renderWithProviders } from "#utils/test-utils";
 
 describe("YourProfile", () => {
   it("renders correctly", () => {
-    const tree = renderWithProviders(<YourProfile />);
-    expect(tree).toMatchSnapshot();
+    renderWithProviders(<YourProfile />);
+    expect(getByText("Phone Number")).toBeInTheDocument();
+    expect(getByText("Email")).toBeInTheDocument();
+    expect(getByText("First Name")).toBeInTheDocument();
+    expect(getByText("Last Name")).toBeInTheDocument();
+    expect(getByText("Country")).toBeInTheDocument();
   });
 });
