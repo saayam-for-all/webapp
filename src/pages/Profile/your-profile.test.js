e305f0fc194843e48e735c1b832c110e7df7a653
Chinmai Sai Kolla <chinmaisaikolla@macbookair.lan>
2025-04-07 14:36:49 -0500

    Updated test cases

--- src/pages/Profile/your-profile.test.js
+++ src/pages/Profile/your-profile.test.js
@@ -1,17 +1,12 @@
 import { render } from "@testing-library/react";
 import YourProfile from "./YourProfile";
 import { MOCK_STATE_LOGGED_IN, renderWithProviders } from "#utils/test-utils";
-import { expect } from "vitest";
 
 // TODO: mock CallModal after adding tests for CallModal
 
 describe("YourProfile", () => {
   it("renders correctly", () => {
-    renderWithProviders(<YourProfile />);
-    expect(getByText("Phone Number")).toBeInTheDocument();
-    expect(getByText("Email")).toBeInTheDocument();
-    expect(getByText("First Name")).toBeInTheDocument();
-    expect(getByText("Last Name")).toBeInTheDocument();
-    expect(getByText("Country")).toBeInTheDocument();
+    const tree = renderWithProviders(<YourProfile />);
+    expect(tree).toMatchSnapshot();
   });
 });
