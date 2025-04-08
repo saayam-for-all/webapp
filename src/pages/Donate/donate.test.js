7833215dd10baa29ab81c03c81b5b050fdb94960
navaneethaenjam <navaneethaenjam9@gmail.com>
2025-04-08 10:38:05 -0500

    updated test-case

--- src/pages/Donate/donate.test.js
+++ src/pages/Donate/donate.test.js
@@ -1,30 +1,9 @@
-//import { render } from "@testing-library/react";
-//import Donate from "./Donate";
-
-//describe("Donate", () => {
-//  it("renders correctly", () => {
-//   const tree = render(<Donate />);
-//   expect(tree).toMatchSnapshot();
-//  });
-//});
-
 import { render } from "@testing-library/react";
-import Donation from "./Donate";
-import { BrowserRouter } from "react-router-dom";
+import Donate from "./Donate";
 
-jest.mock("react-i18next", () => ({
-  useTranslation: () => ({
-    t: (key) => `mockTranslate(${key})`,
-  }),
-}));
-
-describe("Donation Component", () => {
-  it("matches the snapshot", () => {
-    const { asFragment } = render(
-      <BrowserRouter>
-        <Donation />
-      </BrowserRouter>,
-    );
-    expect(asFragment()).toMatchSnapshot();
+describe("Donate", () => {
+  it("renders correctly", () => {
+    const tree = render(<Donate />);
+    expect(tree).toMatchSnapshot();
   });
 });
