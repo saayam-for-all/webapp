3d6fbd87502907333bd644e73285492d363f5fc7
deepika-venkatesan <deepikajvenkatesan@gmail.com>
2025-04-09 12:31:24 -0400

    Update test case

--- src/pages/Profile/your-profile.test.js
+++ src/pages/Profile/your-profile.test.js
@@ -1,34 +1,22 @@
-// import { render } from "@testing-library/react";
-// import YourProfile from "./YourProfile";
-// import { MOCK_STATE_LOGGED_IN, renderWithProviders } from "#utils/test-utils";
-
-// // TODO: mock CallModal after adding tests for CallModal
-
-// describe("YourProfile", () => {
-//   it("renders correctly", () => {
-//     const tree = renderWithProviders(<YourProfile />);
-//     expect(tree).toMatchSnapshot();
-//   });
-// });
 import { render } from "@testing-library/react";
 import YourProfile from "./YourProfile";
 import { renderWithProviders } from "#utils/test-utils";
 
-// Mock i18n
+// mock i18n config
+jest.mock("#common/i18n/i18n", () => ({}));
+
+// react-i18next translation hook
 jest.mock("react-i18next", () => ({
   useTranslation: () => ({
     t: (key) => key,
   }),
 }));
 
-// mock aws-amplify/auth if you suspect issues from there too
+// Mock AWS Amplify Auth functions
 jest.mock("aws-amplify/auth", () => ({
   fetchUserAttributes: jest.fn(() => Promise.resolve([])),
   updateUserAttributes: jest.fn(() => Promise.resolve()),
 }));
-
-// mock CallModal if it causes issues
-jest.mock("./CallModal", () => () => <div>Mocked CallModal</div>);
 
 describe("YourProfile", () => {
   it("renders correctly", () => {
