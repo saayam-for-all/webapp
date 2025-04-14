b22849c71a993f55ebf1d33f2950c12b157823e1
Sahil Gaba <sgaba9778@gmail.com>
2025-04-14 12:16:50 -0400

    Fixed failing test

--- __mocks__/fileTransformer.js
+++
@@ -1,7 +0,0 @@
-import path from "path";
-
-export function process(sourceText, sourcePath, options) {
-  return {
-    code: `module.exports = ${JSON.stringify(path.basename(sourcePath))};`,
-  };
-}
