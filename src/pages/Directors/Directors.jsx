3a2d4f34242ddc18dd3a684c0c8636cd06aa04c7
kimyang1997 <yang.lin1@northeastern.edu>
2025-04-17 03:45:47 +0800

    updated  Directors section with new layout

--- src/pages/Directors/Directors.jsx
+++ src/pages/Directors/Directors.jsx
@@ -12,7 +12,7 @@ import { FaLinkedin } from "react-icons/fa";
 const DirectorsData = [
   {
     name: "Rao K Bhethanabotla",
-    role: "Founder/President/CEO/CTO",
+    role: "Founder, CEO, CTO, President",
     linkedin: "https://www.linkedin.com/in/raobhethanabotla",
     image: RaoImg,
   },
@@ -24,7 +24,7 @@ const DirectorsData = [
   },
   {
     name: "Rao Panuganti",
-    role: "Director & Secretary",
+    role: "Director",
     linkedin: "https://www.linkedin.com/in/rao-panuganti-654443/",
     image: RaoPanugantiImg,
   },
@@ -46,12 +46,16 @@ const Directors = () => {
   const { t } = useTranslation();
   return (
     <div>
-      <h1 className="text-center text-3xl font-semibold">{t("DIRECTORS")}</h1>
+      <h1 className="text-center text-3xl font-bold">{t("DIRECTORS")}</h1>
+      <p className="text-center text-lg text-gray-600 my-4">
+        {t("DIRECTORS_DESCRIPTION")}
+      </p>
+
       <div className="directors flex items-center justify-center flex-wrap gap-10">
         {DirectorsData.map((director, index) => (
           <div
             key={index}
-            className="director h-[500px] w-[350px] p-4 m-4 flex flex-col items-center justify-center text-center shadow-lg hover:shadow-2xl hover:scale-110 transition-transform duration-500 ease-in-out rounded-xl"
+            className="director h-[500px] w-[350px] flex flex-col items-center justify-center text-center hover:shadow-2xl hover:scale-110 transition-transform duration-500 ease-in-out rounded-xl"
           >
             <div>
               <img
@@ -65,16 +69,19 @@ const Directors = () => {
                 className="rounded-lg hover:rounded-2xl transition-all shadow-lg"
               />
             </div>
-            <h2 className="flex gap-2 mt-5">
-              <a
-                href={director.linkedin}
-                className="text-blue-500"
-                target="_blank"
-              >
-                {director.name}
-              </a>
-            </h2>
-            <h3 className="flex gap-2 mt-5">{director.role}</h3>
+
+            <div className="text-left self-start w-full px-6">
+              <h2 className="flex gap-2 mt-5">
+                <a
+                  href={director.linkedin}
+                  className="text-black font-bold text-xl"
+                  target="_blank"
+                >
+                  {director.name}
+                </a>
+              </h2>
+              <h3 className="flex gap-2">{director.role}</h3>
+            </div>
           </div>
         ))}
       </div>
