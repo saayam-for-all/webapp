b446671a56006458a47b0e5ec7eb9f9df73473b6
kimyang1997 <yang.lin1@northeastern.edu>
2025-04-19 00:23:59 +0800

    fix the snapshot issue

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,13 @@
 export default {
   transform: {
     "^.+\\.jsx?$": "babel-jest",
-    "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/__mocks__/fileTransformer.js",
+    // "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/__mocks__/fileTransformer.js",
   },
   transformIgnorePatterns: ["node_modules/(?!(node-fetch)/)"],
   testEnvironment: "jsdom",
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|svg|webp|ico|bmp)$":
+      "<rootDir>/__mocks__/fileMock.js",
   },
 };
