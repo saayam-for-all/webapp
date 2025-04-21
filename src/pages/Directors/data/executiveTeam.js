3f77dcc57709791ef9a305c901781bd739347518
pavankumarreddykasarla <pavan.kasrla7@gmail.com>
2025-04-20 19:47:35 -0700

    Executive team Page 0.5mvp

---
+++ src/pages/Directors/data/executiveTeam.js
@@ -0,0 +1,87 @@
+const ExecutiveTeamData = [
+  {
+    image:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/42afdfb974265d5ede7e4b08aeff12e6a278dbec",
+    name: "Rao K Bhethanabotla",
+    role: "Founder, CEO, CTO, President",
+  },
+  {
+    image:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/cd09abc1a5a391a8775f93bf52dc1c7bd3bde48c",
+    name: "Rishita Rapuri",
+    role: "Human Resources Manager",
+  },
+  {
+    image:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/42134acaa608afdb1f30b2328f3ef05c63c7aee1",
+    name: "Rashmi",
+    role: "Lead Application Developer",
+  },
+  {
+    image:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/8f181836a0df98180bc82bea522f96509eea4a8d",
+    name: "Sriram Mahankali",
+    role: "Design and Creative Lead",
+  },
+  {
+    image:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/f1155d03bc35b0497b2368237a95f6943836844a",
+    name: "Shubam",
+    role: "AI/ML Team Lead",
+  },
+  {
+    image:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/e2fa182bfa332d6844de8112c9d522f03b2c678f",
+    name: "Aatish Kayyath",
+    role: "------",
+  },
+  {
+    image:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/ce8a2aa126df5bac3884b96f7fe05a41fba85b38",
+    name: "Divya Tanwar",
+    role: "-----",
+  },
+  {
+    image:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/e1f54ababf3e0deebac0fef1e47e682a9823e34e",
+    name: "Kavyasree",
+    role: "Lead Project Manager",
+  },
+  {
+    image:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/6b58758ef77efe32c301deac6f4f0c1f6abb8fce",
+    name: "Purva Patil",
+    role: "Lead Product Manager",
+  },
+  {
+    image:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/92f637ee67b17eef71df92364e32211645c6e3d0",
+    name: "Mohana Krishanan R.",
+    role: "-----",
+  },
+  {
+    image:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/f0e403a7bcfa9d9be2f1c66f6fac4ca2218935ea",
+    name: "Rishab Ghosh",
+    role: "-----",
+  },
+  {
+    image:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/bcb73fdde6f2bca35c1db86c6ecb36f58fcd4fae",
+    name: "Sugandha Agarwal",
+    role: "-----",
+  },
+  {
+    image:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/b6c938fe9f544ef0dacab7bd69eaca9a1a890fdb",
+    name: "Uma",
+    role: "-----",
+  },
+  {
+    image:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/091aca13a6e830ee2374f73b8d227506b2a90566",
+    name: "Meghana Karri",
+    role: "UX Lead",
+  },
+];
+export default ExecutiveTeamData;
