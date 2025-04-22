fb7ae298ee9ddcbc913893f7c28386174a2d4f79
Sahil Gaba <sgaba9778@gmail.com>
2025-04-22 12:23:21 -0400

    Fixed a minor import bug

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,7 @@
 import Login from "../pages/Auth/Login";
 import SignUp from "../pages/Auth/Signup";
 import OTPVerification from "../pages/Auth/VerifyOtp";
+import Collaborators from "../pages/Collaborators/Collaborators";
 import Dashboard from "../pages/Dashboard/Dashboard";
 import Directors from "../pages/Directors/Directors";
 import Donate from "../pages/Donate/Donate";
