cd5007e4ba69c0ac8d55542d4d10ce7688a3cd02
moddebaixa <moddebaixa@gmail.com>
2025-05-15 17:56:12 +0200

    pipeline test

---
+++ nightwatch.conf.js
@@ -0,0 +1,47 @@
+module.exports = {
+  src_folders: ['test', 'nightwatch/examples'],
+  page_objects_path: ['nightwatch/page-objects'],
+  custom_commands_path: ['nightwatch/custom-commands'],
+  custom_assertions_path: ['nightwatch/custom-assertions'],
+  plugins: ['nightwatch-axe-verbose'],
+  test_workers: { enabled: true },
+  test_settings: {
+    default: {
+      launch_url: 'http://localhost',
+      screenshots: { enabled: false, path: 'screens', on_failure: true },
+      desiredCapabilities: { browserName: 'chrome' },
+      webdriver: {
+        start_process: true,
+        server_path: '/usr/bin/chromedriver'
+      }
+    },
+    firefox: {
+      desiredCapabilities: {
+        browserName: 'firefox',
+        alwaysMatch: {
+          acceptInsecureCerts: true,
+          'moz:firefoxOptions': { args: [] }
+        }
+      },
+      webdriver: {
+        start_process: true,
+        server_path: '/usr/bin/geckodriver'
+      }
+    },
+    chrome: {
+      desiredCapabilities: {
+        browserName: 'chrome',
+        'goog:chromeOptions': { w3c: true, args: [] }
+      },
+      webdriver: {
+        start_process: true,
+        server_path: '/usr/bin/chromedriver'
+      }
+    }
+  },
+  usage_analytics: {
+    enabled: true,
+    log_path: './logs/analytics',
+    client_id: 'a48eaa29-d1f1-4187-919e-d995cc7e2cf0'
+  }
+};
