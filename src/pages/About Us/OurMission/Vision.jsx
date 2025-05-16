435288d30bf37d1649f1d66474e9238732e0dc90
pavankumarreddykasarla <pavan.kasrla7@gmail.com>
2025-05-15 19:37:24 -0700

    Chnages in the mission page

--- src/pages/About Us/OurMission/Vision.jsx
+++ src/pages/About Us/OurMission/Vision.jsx
@@ -19,22 +19,22 @@ export function Vision() {
           <img
             src="https://cdn.builder.io/api/v1/image/assets/TEMP/9e489c91f28508597741986fff385e42dcf07f04"
             alt="Vision image 1"
-            className="object-cover h-[150px] w-[70%] ml-[50px]" // 👈 aligned right
+            className="object-cover h-[150px] w-[95%] md:w-[70%] ml-[10px] md:ml-[50px] rounded-tl-[20px]  md:rounded-tl-none" // 👈 aligned right
           />
           <img
             src="https://cdn.builder.io/api/v1/image/assets/TEMP/96f2577da3ff33600f091becd5353f37e9ec91fd"
             alt="Vision image 2"
-            className="object-cover h-[210px] w-full md:ml-[-20px]"
+            className="object-cover h-[210px] w-full md:ml-[-20px] rounded-tr-[20px]  md:rounded-tr-none"
           />
           <img
             src="https://cdn.builder.io/api/v1/image/assets/TEMP/d69a9a1b73ece3f214ca700fdad2926ac86ffb47"
             alt="Vision image 3"
-            className="object-cover h-[150px] w-[70%] mt-[-50px] ml-[50px]" // 👈 aligned right
+            className="object-cover h-[150px] w-[95%] md:w-[70%] mt-[-50px] ml-[10px] md:ml-[50px] rounded-bl-[20px]  md:rounded-bl-none" // 👈 aligned right
           />
           <img
             src="https://cdn.builder.io/api/v1/image/assets/TEMP/7eed748020e8f83d968948c1754918c0f51ff363"
             alt="Vision image 4"
-            className="object-cover h-[100px] w-full md:ml-[-20px]"
+            className="object-cover h-[100px] w-full md:ml-[-20px] rounded-br-[20px]  md:rounded-br-none"
           />
         </div>
       </div>
