3ca4ea8b864c990f07afc01b4a4ff9f9407a5342
moddebaixa <moddebaixa@gmail.com>
2025-05-22 21:02:00 +0200

    firefox test

--- nightwatch.conf.cjs
+++ nightwatch.conf.cjs
@@ -42,7 +42,7 @@ module.exports = {
       },
       webdriver:   {
      start_process: true,
-     server_path: 'node_modules/.bin/chromedriver',
+     server_path: '/usr/bin/chromedriver',
      port: 9515,
      host: 'localhost',
      ssl: false,
