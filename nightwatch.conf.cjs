6bc13a2b2f442978d8c99f8add2906cd59e49fa7
moddebaixa <moddebaixa@gmail.com>
2025-05-23 14:21:52 +0200

    chrome test

--- nightwatch.conf.cjs
+++ nightwatch.conf.cjs
@@ -3,10 +3,11 @@ module.exports = {
   page_objects_path: ['nightwatch/page-objects'],
   custom_commands_path: ['nightwatch/custom-commands'],
   custom_assertions_path: ['nightwatch/custom-assertions'],
-   plugins: 
-    ['nightwatch-axe-verbose'],
-    
+  
+  plugins: ['nightwatch-axe-verbose'],
+  
   test_workers: { enabled: true },
+  
   test_settings: {
     default: {
       launch_url: 'http://localhost',
@@ -17,41 +18,64 @@ module.exports = {
         server_path: '/usr/local/bin/chromedriver'
       }
     },
-     firefox: {
+
+    firefox: {
       desiredCapabilities: {
         browserName: 'firefox',
         'moz:firefoxOptions': {
           args: ['-headless']
         }
       },
-      webdriver:  {
-     start_process: true,
-     server_path: 'node_modules/.bin/geckodriver',
-     port: 4444,
-     host: 'localhost',
-     ssl: false,
-     default_path_prefix: '',
-     proxy: undefined,
-     cli_args: [ '-vv' ]
-  }
- 
-    }, chrome: {
+      webdriver: {
+        start_process: true,
+        server_path: 'node_modules/.bin/geckodriver',
+        port: 4444,
+        host: 'localhost',
+        ssl: false,
+        default_path_prefix: '',
+        proxy: undefined,
+        cli_args: ['-vv']
+      }
+    },
+
+    chrome: {
       desiredCapabilities: {
         browserName: 'chrome',
-        'goog:chromeOptions': { w3c: true, args: ['-headless'] }
+        'goog:chromeOptions': {
+          w3c: true,
+          args: [
+            '--headless=new',
+            '--disable-gpu',
+            '--no-sandbox',
+            '--disable-dev-shm-usage',
+            `--user-data-dir=/tmp/chrome-profile-${Date.now()}`
+          ]
+        }
       },
-      webdriver:       {
-     start_process: true,
-     server_path: './usr/bin/chromedriver',
-     port: undefined,
-     host: 'localhost',
-     ssl: false,
-     default_path_prefix: '',
-     proxy: undefined,
-     cli_args: [ '-vv' ]
-  }
+      webdriver: {
+        start_process: true,
+        server_path: './usr/bin/chromedriver',
+        port: undefined,
+        host: 'localhost',
+        ssl: false,
+        default_path_prefix: '',
+        proxy: undefined,
+        cli_args: [
+          '--verbose',
+          '--log-path=logs/chromedriver.log',
+          '-vv'
+        ]
+      },
+      output: true,
+      disable_colors: false,
+      detailed_output: true,
+      live_output: true,
+      globals: {
+        waitForConditionTimeout: 10000
+      }
     }
   },
+
   usage_analytics: {
     enabled: true,
     log_path: './logs/analytics',
