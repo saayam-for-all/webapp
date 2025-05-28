850894fb41c7d3ab76ed38e34256945b42a93c9c
Manish Malepati <mr.malepati@gmail.com>
2025-05-28 01:22:43 -0400

    OurMission and HowWeOperate open at the top

--- src/pages/About Us/OurMission/OurMission.jsx
+++ src/pages/About Us/OurMission/OurMission.jsx
@@ -1,9 +1,14 @@
+import { useEffect } from "react";
 import { MissionHero } from "./MissionHero";
 import { Mission } from "./Mission";
 import { Vision } from "./Vision";
 import { JoinCTA } from "./JoinCTA";
 
 export default function OurMission() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <MissionHero />
