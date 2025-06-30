c6252169c98507213b73a4506d3b976ed368943b
Ganesh Pillala <ganesh.p11197@gmail.com>
2025-06-30 01:44:24 -0400

    Alignment myteampage (#701)
    * Adjust the grid of directors page and removal of executive team members
    
    * updated snapshot

--- src/pages/Directors/data/executiveTeam.js
+++ src/pages/Directors/data/executiveTeam.js
@@ -1,7 +1,5 @@
 import Rishita_Rapuri from "../../../assets/images/Rishitha Rapuri.jpg";
 import Rashmi_Bilaskar from "../../../assets/images/Rashmi Bilaskar.jpeg";
-import Shubham_Narkhede from "../../../assets/images/Shubham Narkhede.jpg";
-import Kavyasree_Bommakanti from "../../../assets/images/Kavyasree Bommakanti.jpg";
 import Meghana_Karri from "../../../assets/images/Meghana Karri.jpeg";
 import Sunandha_agarwal from "../../../assets/images/Sunandha_agarwal.jpg";
 import Mohanakrishnan from "../../../assets/images/Mohanakrishnan.jpg";
@@ -22,24 +20,14 @@ const ExecutiveTeamData = [
     role: "Business Analyst Team Lead",
     linkedin: "https://www.linkedin.com/in/rishitha-rapuri-a59a311a6/",
   },
-  {
-    image: Kavyasree_Bommakanti,
-    name: "Kavyasree Bommakanti",
-    role: "Program Manager | Business Analyst",
-    linkedin: "https://www.linkedin.com/in/kavyasree-b-b4941b299/",
-  },
+
   {
     image: Sriram_Mahankali,
     name: "Sriram Mahankali",
     role: "Design and Creative Lead",
     linkedin: "https://www.linkedin.com/in/sriram-mahankali-393047342",
   },
-  {
-    image: Shubham_Narkhede,
-    name: "Shubham Narkhede",
-    role: "AI/ML Team Lead",
-    linkedin: "https://www.linkedin.com/in/shubhamnarkhede",
-  },
+
   // {
   //   image: "https://cdn.builder.io/api/v1/image/assets/TEMP/e2fa182bfa332d6844de8112c9d522f03b2c678f",
   //   name: "Aatish Kayyath",
