9cbad3ed2a54f3456dc665db0a27b18efcbed724
shuchi mandal <shuchi1999@gmail.com>
2025-08-12 21:43:45 -0500

    Sort Categories and added textbox

--- src/pages/Volunteer/steps/Skills.jsx
+++ src/pages/Volunteer/steps/Skills.jsx
@@ -1,4 +1,5 @@
-import React from "react"; //added for testing
+import React, { useState } from "react"; //added for testing
+import { useTranslation } from "react-i18next";
 
 // Example JSON data with multiple levels of subCategories - need to get this data from server
 const Skills = ({
@@ -6,9 +7,17 @@ const Skills = ({
   setCheckedCategories,
   categoriesData,
 }) => {
+  const [text, setText] = useState("");
+  const { t } = useTranslation();
   // Recursive function to handle rendering categories and subcategories
   const renderCategories = (categories, parentPath = "") => {
-    return categories.map((cat, index) => {
+    const sortedCategories = [...categories].sort((a, b) => {
+      const aName = typeof a === "object" ? a.category : a;
+      const bName = typeof b === "object" ? b.category : b;
+      return aName.localeCompare(bName);
+    });
+
+    return sortedCategories.map((cat, index) => {
       const isObject = typeof cat === "object";
       const categoryName = isObject ? cat.category : cat;
       const hasSubCategories = isObject && cat.subCategories;
@@ -31,6 +40,26 @@ const Skills = ({
               {categoryName}
             </span>
           </label>
+
+          {currentPath === "General" && getCheckedStatus(currentPath) && (
+            <div className="ml-5 mt-2">
+              <label
+                htmlFor="description"
+                className="block text-gray-700 font-medium mb-2"
+              >
+                {t("DESCRIPTION")}
+                <span className="text-red-500 m-1">*</span>(
+                {t("MAX_CHARACTERS", { count: 500 })})
+              </label>
+              <textarea
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                placeholder="Please enter the details here"
+                maxLength={500}
+                className="border p-2 w-full rounded-lg"
+              ></textarea>
+            </div>
+          )}
 
           {/* Recursively render subcategories if they exist */}
           {hasSubCategories && getCheckedStatus(currentPath) && (
