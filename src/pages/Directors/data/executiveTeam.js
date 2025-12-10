5e35c8af9fa0126c84b3e8d10c56292aa5de2de6
Ganesh Pillala <ganesh.p11197@gmail.com>
2025-12-10 02:29:10 -0500

    Updated Executive Team Data (#1128)
    * Updated Executive Team Data
    
    * updated executive test team

--- src/pages/Directors/data/executiveTeam.js
+++ src/pages/Directors/data/executiveTeam.js
@@ -5,7 +5,7 @@ import Sunandha_agarwal from "../../../assets/images/Sunandha_agarwal.jpg";
 import Mohanakrishnan from "../../../assets/images/Mohanakrishnan.jpg";
 import Divya_Tanwar from "../../../assets/images/Divya Tanwar.jpg";
 import Sriram_Mahankali from "../../../assets/images/Sriram_Mahankali.jpeg";
-
+import Satya_Sai_Manoj from "../../../assets/images/Satya_Sai_Manoj.jpeg";
 const ExecutiveTeamData = [
   {
     image: Rashmi_Bilaskar,
@@ -80,6 +80,12 @@ const ExecutiveTeamData = [
     name: "Meghana Karri",
     role: "Lead User Experience Designer",
     linkedin: "https://www.linkedin.com/in/meghanakarri/",
+  },
+  {
+    image: Satya_Sai_Manoj,
+    name: "Satya Sai Manoj",
+    role: "Strategy and Project Management Lead",
+    linkedin: "https://www.linkedin.com/in/satya-sai-manoj-m-403179230/",
   },
 ];
 
