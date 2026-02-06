b35f0a24b5ca5e88fcafccc2636df68d1b858234
vaishnavi2212 <61548701+vaishnavi2212@users.noreply.github.com>
2026-02-06 16:18:43 -0600

    Req Details added to english

--- src/pages/RequestDetails/RequestDetails.jsx
+++ src/pages/RequestDetails/RequestDetails.jsx
@@ -81,6 +81,9 @@ const RequestDetails = () => {
         </button>
       </div>
 
+      <div className="w-full px-4 mb-4">
+        <h1 className="text-3xl font-bold">{t("REQUEST_DETAILS")}</h1>
+      </div>
       <div className="m-8 grid grid-cols-13 gap-4">
         {!requestData ? (
           <div>Loading...</div>
