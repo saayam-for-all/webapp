4093a2d8db19aa2b36a2a1f3e8623f30cbed913f
SATHIYASHIVANI <sathishkumarsathiya@cityuniversity.com>
2026-02-10 14:51:32 -0800

    fix: hide delete button from first availability row in volunteer form

--- src/pages/Volunteer/steps/Availability.jsx
+++ src/pages/Volunteer/steps/Availability.jsx
@@ -71,31 +71,33 @@ const TimeInputComponent = ({
         minutePlaceholder="mm"
         clearIcon={null}
       />
-      <button
-        className="border border-red-500 rounded-full hover:bg-red-100"
-        onClick={() => onRemove(index)}
-      >
-        <svg
-          className="w-3 h-3"
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          width="100"
-          height="100"
-          viewBox="0 0 48 48"
+      {index > 0 && (
+        <button
+          className="border border-red-500 rounded-full hover:bg-red-100"
+          onClick={() => onRemove(index)}
         >
-          <path
-            fill="#F44336"
-            d="M21.5 4.5H26.501V43.5H21.5z"
-            transform="rotate(45.001 24 24)"
-          ></path>
-          <path
-            fill="#F44336"
-            d="M21.5 4.5H26.5V43.501H21.5z"
-            transform="rotate(135.008 24 24)"
-          ></path>
-        </svg>
-      </button>
+          <svg
+            className="w-3 h-3"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="100"
+            height="100"
+            viewBox="0 0 48 48"
+          >
+            <path
+              fill="#F44336"
+              d="M21.5 4.5H26.501V43.5H21.5z"
+              transform="rotate(45.001 24 24)"
+            ></path>
+            <path
+              fill="#F44336"
+              d="M21.5 4.5H26.5V43.501H21.5z"
+              transform="rotate(135.008 24 24)"
+            ></path>
+          </svg>
+        </button>
+      )}
     </div>
   );
 };
