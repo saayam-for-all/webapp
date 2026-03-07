353e2dc13be0bb900df0e4d5138afff23fa47fef
Rashmi <30849275+rmb2020@users.noreply.github.com>
2026-03-06 20:25:02 -0800

    Fixes adsense layout for login, signup, contact

--- src/common/components/Ads/VerticalAd.jsx
+++ src/common/components/Ads/VerticalAd.jsx
@@ -3,7 +3,9 @@ import { useEffect } from "react";
 const VerticalAd = () => {
   useEffect(() => {
     try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
+      if (window.location.hostname === "saayamforall.org") {
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      }
     } catch (e) {
       console.error("Adsense error", e);
     }
