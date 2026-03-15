d7fc5e5452859dc94f0ccb17be8045c8234c7dd6
deekshit18 <deekshitsrihari.gd@gmail.com>
2026-03-15 18:37:24 -0400

    Fix breadcrumb CSS import case

--- src/common/components/BreadCrumbs/BreadCrumbs.jsx
+++ src/common/components/BreadCrumbs/BreadCrumbs.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import "./breadcrumbs.css";
+import "./BreadCrumbs.css";
 
 const Breadcrumbs = () => {
   const location = useLocation();
