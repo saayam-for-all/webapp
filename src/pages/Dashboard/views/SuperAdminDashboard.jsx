9ff72ccc1d1feb568e98f763ca76aea70754d3a4
Aniket Kumar <43331966+Anikkk@users.noreply.github.com>
2026-07-18 17:31:53 -0700

    Add bulk status update and checkboxes to Admin and Super Admin request dashboards

--- src/pages/Dashboard/views/SuperAdminDashboard.jsx
+++ src/pages/Dashboard/views/SuperAdminDashboard.jsx
@@ -25,6 +25,9 @@ const SuperAdminDashboard = (props) => {
     setAnalyticsSubtab,
     serverPaginated,
     serverTotalRows,
+    selectedRequestIds,
+    onSelectRequest,
+    onSelectAllRequests,
   } = props;
 
   return (
@@ -135,6 +138,10 @@ const SuperAdminDashboard = (props) => {
             getLinkPath={getLinkPath}
             getLinkState={getLinkState}
             serverPaginated={serverPaginated}
+            selectable
+            selectedIds={selectedRequestIds}
+            onSelectRow={onSelectRequest}
+            onSelectAllRows={onSelectAllRequests}
           />
         )}
       </div>
@@ -162,6 +169,9 @@ SuperAdminDashboard.propTypes = {
   setAnalyticsSubtab: PropTypes.func,
   serverPaginated: PropTypes.bool,
   serverTotalRows: PropTypes.number,
+  selectedRequestIds: PropTypes.array,
+  onSelectRequest: PropTypes.func,
+  onSelectAllRequests: PropTypes.func,
 };
 
 export default SuperAdminDashboard;
