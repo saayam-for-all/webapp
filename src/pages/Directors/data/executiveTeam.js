7fe23e9ae9cd0d4dd13338fe6956f986b45b4a88
Hari Chandana Kasula <harichandanakasula@gmail.com>
2026-08-18 00:35:02 +0530

    feat: add 3 executive profiles to Our Team page (#1767)

--- src/pages/Directors/data/executiveTeam.js
+++ src/pages/Directors/data/executiveTeam.js
@@ -1,6 +1,9 @@
 import Rashmi_Bilaskar from "../../../assets/images/Rashmi Bilaskar.jpeg";
 import Sunandha_agarwal from "../../../assets/images/Sunandha_agarwal.jpg";
 import Aakash_Gangji from "../../../assets/images/Aakash_Gangji.jpg";
+import Jeevan_Deep_Borugadda from "../../../assets/images/Jeevan_Deep_Borugadda.webp";
+import Sana_Desai from "../../../assets/images/Sana_Desai.jpg";
+import Sahil_Mane from "../../../assets/images/Sahil_Mane.webp";
 
 const ExecutiveTeamData = [
   {
@@ -16,6 +19,24 @@ const ExecutiveTeamData = [
     role: "Director of Engineering",
     linkedin:
       "https://www.linkedin.com/in/agrasugandha/?trk=opento_sprofile_details",
+  },
+  {
+    image: Jeevan_Deep_Borugadda,
+    name: "Jeevan Deep Borugadda",
+    role: "Program and Operations Lead",
+    linkedin: "https://www.linkedin.com/in/jeevandeepb/",
+  },
+  {
+    image: Sana_Desai,
+    name: "Sana Desai",
+    role: "Lead Data Engineer",
+    linkedin: "https://www.linkedin.com/in/sana-desai-4bb185146/",
+  },
+  {
+    image: Sahil_Mane,
+    name: "Sahil Mane",
+    role: "Data Analytics & Engineering Lead",
+    linkedin: "https://www.linkedin.com/in/sahil-mane-502a161aa/",
   },
 ];
 
